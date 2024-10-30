78aa7cb5e51736fcc5d61332c15b2c1d7cf62d7e
SaurabhPrajapati21 <saurabhprajapati7378@gmail.com>
2024-10-30 19:43:51 +0530

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,6 @@ import footerLogo from "../../assets/women/logo.png";
 import Banner from "../../assets/website/footer-img.jpg";
 import { FiMail } from "react-icons/fi";
 import { FaGithub } from "react-icons/fa";
-
-
-
 import {
   FaInstagram,
   FaLinkedin,
@@ -66,11 +63,15 @@ const FooterLinks = [
     // link: "/#Mail",
   },
 ];
+
 const Footer = () => {
   return (
     <div style={BannerImg} className="text-white mt-0 min-h-[320px]">
       <div className="container px-4 md:px-10">
-        <div data-aos="zoom-in" className="grid md:grid-cols-3 pb-20 pt-5">
+        <div
+          data-aos="zoom-in"
+          className="grid md:grid-cols-3 pb-10 pt-5 sm:pb-20"
+        >
           {/* Company Details */}
           <div className="py-8 px-4">
             <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
@@ -81,7 +82,7 @@ const Footer = () => {
               />
               NovaStore
             </h1>
-            <p>
+            <p className="text-sm sm:text-base">
               NovaStore – Bringing quality and convenience to your doorstep.
               Shop with confidence, discover top brands, and enjoy exceptional
               service every time.
@@ -91,13 +92,13 @@ const Footer = () => {
           {/* Footer Links */}
           <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
             <div className="py-8 px-4">
-              <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+              <h1 className="sm:text-xl text-lg font-bold sm:text-left text-justify mb-3">
                 Important Links
               </h1>
               <ul className="flex flex-col gap-3">
                 {FooterLinks.map((link) => (
                   <li key={`important-${link.title}`}>
-                    <span className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-gray-200">
+                    <span className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-gray-200 text-sm sm:text-base">
                       {link.title}
                     </span>
                   </li>
@@ -105,13 +106,13 @@ const Footer = () => {
               </ul>
             </div>
             <div className="py-8 px-4">
-              <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+              <h1 className="sm:text-xl text-lg font-bold sm:text-left text-justify mb-3">
                 Consumers Policy
               </h1>
               <ul className="flex flex-col gap-3">
                 {Consumer.map((link) => (
                   <li key={`additional-${link.title}`}>
-                    <span className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-gray-200">
+                    <span className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-gray-200 text-sm sm:text-base">
                       {link.title}
                     </span>
                   </li>
@@ -128,7 +129,7 @@ const Footer = () => {
                   aria-label="Linkedin"
                   target="_blank"
                 >
-                  <FaLinkedin className="text-3xl" />
+                  <FaLinkedin className="text-2xl sm:text-3xl" />
                 </a>
                 <a
                   href="https://github.com/SaurabhPrajapati21"
@@ -136,7 +137,7 @@ const Footer = () => {
                   aria-label="GitHub"
                   target="_blank"
                 >
-                  <FaGithub className="text-3xl" />
+                  <FaGithub className="text-2xl sm:text-3xl" />
                 </a>
 
                 <a
@@ -145,21 +146,26 @@ const Footer = () => {
                   aria-label="Instagram"
                   target="_blank"
                 >
-                  <FaInstagram className="text-3xl" />
+                  <FaInstagram className="text-2xl sm:text-3xl" />
                 </a>
               </div>
               <div className="mt-6">
                 <div className="flex items-center gap-3">
-                  <FaLocationArrow />
-                  <p>Nagpur, Maharashtra - 441108</p>
+                  <FaLocationArrow className="text-xl sm:text-2xl" />
+                  <p className="text-sm sm:text-base">
+                    Nagpur, Maharashtra - 441108
+                  </p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
-                  <FaMobileAlt />
-                  <p>+91 9637380920</p>
+                  <FaMobileAlt className="text-xl sm:text-2xl" />
+                  <p className="text-sm sm:text-base">+91 9637380920</p>
                 </div>
-                <div className="flex items-center gap-3 mt-3">
-                  <FiMail />
-                  <p>saurabhprajapati7378@gmail.com</p>
+
+                <div className="flex flex-wrap items-center gap-3 mt-3">
+                  <FiMail className="text-xl sm:text-2xl" />
+                  <p className="text-sm sm:text-base">
+                    saurabhprajapati7378@gmail.com
+                  </p>
                 </div>
               </div>
             </div>
@@ -167,7 +173,7 @@ const Footer = () => {
         </div>
 
         {/* Footer Note */}
-        <div className="border-t border-gray-600 py-4 text-center text-gray-300 text-sm">
+        <div className="border-t border-gray-600 py-2 text-center text-gray-300 text-sm">
           © 2024 Saurabh Prajapati. All rights reserved.
         </div>
       </div>
